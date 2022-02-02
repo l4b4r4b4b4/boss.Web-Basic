df6f2256ba0de581c6d5d5123feef5a8ebccb234
l4b4r4b4b4 <lucas.cansino@mail.de>
2022-02-02 18:39:29 +0100

    cleaned docker-compose.yml

--- redis/config.php
+++ redis/config.php
@@ -1,4 +1,4 @@
 define('WP_REDIS_HOST', 'redis');
 define('WP_REDIS_PORT', 6379);
 define('WP_CACHE_KEY_SALT','praxTTNijEApV0pGFQcpRknIXOOoNKyylA3i');
-define('WP_REDIS_PASSWORD', 'NDpRxZ52TZk7x5gdrbwyKNcfAELUjqm7sTVNFMjhMGLhwYXi2ZZVht');
+define('WP_REDIS_PASSWORD', 's3986h3ja6hs03j8i80zak08337j8p');
